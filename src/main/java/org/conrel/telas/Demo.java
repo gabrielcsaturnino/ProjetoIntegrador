5fdee74866ae2bf6fd525788487152162945ec5a
tuladelc <tuladel.123.team2020@hotmail.com>
2023-10-22 18:54:25 -0300

    v1

---
+++ src/main/java/org/conrel/telas/Demo.java
@@ -0,0 +1,46 @@
+package org.conrel.telas;
+
+import org.conrel.dal.ConnectDB;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Demo extends JFrame{
+
+    private JLabel nome;
+    private JTextField textName;
+    private JButton button;
+    private JPanel mainPanel;
+
+    public Demo() {
+        setContentPane(mainPanel);
+        setTitle("Gui teste");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(200, 300);
+        setLocationRelativeTo(null);
+        setVisible(true);
+
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(Demo.this, textName.getText() + " Hello");
+            }
+        });
+    }
+
+    public static void main(String[] args) throws SQLException {
+        //new Demo();
+
+        ConnectDB db = new ConnectDB();
+
+        db.consulta();
+
+    }
+}
+
+
+
+
