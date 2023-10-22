c013de65b19b109a2fa6e3ae46767556bd0648cc
tuladelc <tuladel.123.team2020@hotmail.com>
2023-10-22 18:54:25 -0300

    v1

---
+++ src/main/java/org/conrel/dal/ConnectDB.java
@@ -0,0 +1,44 @@
+package org.conrel.dal;
+
+import java.sql.*;
+
+public class ConnectDB {
+    Connection con;
+   public ConnectDB(){
+
+       try {
+            con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/DBPROJETO",
+                   "postgres", "1234");
+           if (con != null){
+               System.out.println("Banco conectado");
+
+           }else {
+               System.out.println("Conexão falhou!");
+           }
+       }catch (Exception e){
+           System.out.println(e.getMessage());
+       }
+
+
+   }
+
+   public void consulta() throws SQLException {
+       try {
+
+           Statement result = con.createStatement();
+
+           ResultSet st = result.executeQuery("SELECT nome, cargo, salario_hora FROM FUNCIONARIO");
+           while (st.next()){
+           System.out.println("nome: " + st.getString("nome") + "\ncargo: " + st.getString("cargo") + "\nsalario:" + st.getInt("salario_hora")) ;}
+       }catch (Exception e){
+           System.out.println(e.getMessage());
+       }
+       con.close();
+   }
+
+
+
+
+
+
+}
